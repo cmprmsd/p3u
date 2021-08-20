f2961c1a671d921c7e6e0fd54de4a40c1b9cb553
Nico Jansen <9384332+0x483d@users.noreply.github.com>
2021-08-20 17:56:36 +0200

    Added logging for failed authentication

--- p3u.py
+++ p3u.py
@@ -11,27 +11,48 @@ import cgi
 import http.server
 import ssl
 import string
+from datetime import datetime
 
 __all__ = ["P3U"]
 __author__ = "H8.to"
 __home_page__ = "http://h8.to/"
 
+auth_logfile = open("auth_log.txt", "a")
+
 # This script makes all arguments optional and uses C style arguments
 # Original script: https://github.com/4d4c/http.server_upload/blob/master/https_upload.py
 # This version also includes path traversal fix, host argument and certificate generate argument -g
 
-class CustomBaseHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
-    server_version = "P3U"  # replaces BaseHTTP/0.6
+class SekurWebServer(http.server.BaseHTTPRequestHandler):
+    server_version = "Apache"  # replaces BaseHTTP/0.6
     sys_version = ""  # replaces Python/3.6.7
 
+    # Check if user is authenticated
     def is_authenticated(self):
         if use_auth:
             authorization_header = self.headers["Authorization"]
-            if authorization_header != self.basic_authentication_key:
-                self.do_AUTHHEAD()
-                self.close_connection = True
-                return False
+            if authorization_header == self.basic_authentication_key:
+                # Allow access if password is correct
+                return True
+            
+            # If password is wrong, log failed authentication to stdout
+            if authorization_header is not None:
+                time = datetime.now().strftime("[%d/%b/%Y %H:%M:%S]")
+                source_ip = self.client_address[0]
+                
+                try:
+                    b64 = authorization_header.replace("Basic ", "")
+                    provided_data = base64.b64decode(b64).decode('utf-8')
+                except:
+                    provided_data = str(authorization_header)
 
+                # Log failed authentication
+                auth_logfile.write(source_ip + " - - " + time + " INVALID_LOGON_ATTEMPT (" + provided_data + ")\n")
+
+            self.do_AUTHHEAD()
+            self.close_connection = True
+            return False
+        # If no authentication enabled, allow access
         return True
 
     def do_AUTHHEAD(self):
@@ -58,7 +79,7 @@ class CustomBaseHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
                 "CONTENT_TYPE": self.headers['Content-Type']
             }
         )
-
+        # Save File after uploading it.
         dir_path = os.getcwd()
         file_name = urllib.parse.unquote(post_form["file"].filename)
         valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
@@ -72,7 +93,6 @@ class CustomBaseHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             return
 
         request_path = os.getcwd() + re.split(r'\?|#', self.path)[0]
-
         if os.path.isdir(request_path):
             directory_contents_html = self.get_directory_contents_html(request_path)
 
@@ -81,7 +101,6 @@ class CustomBaseHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_header("Content-Length", str(len(directory_contents_html)))
             self.end_headers()
             self.wfile.write(directory_contents_html)
-
             return
 
         try:
@@ -148,11 +167,12 @@ class CustomBaseHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             </html>
         """.format(request_path, file_list_html).encode()
 
+# Setup Server (CustomBaseHTTPR)
 def start_https_server(listening_port, basic_authentication_key, certificate_file):
     if use_auth:
-        CustomBaseHTTPRequestHandler.basic_authentication_key = "Basic " + basic_authentication_key.decode("utf-8")
+        SekurWebServer.basic_authentication_key = "Basic " + basic_authentication_key.decode("utf-8")
 
-    https_server = http.server.HTTPServer((host, listening_port), CustomBaseHTTPRequestHandler)
+    https_server = http.server.HTTPServer((host, listening_port), SekurWebServer)
     if certificate_file:
         https_server.socket = ssl.wrap_socket(https_server.socket, certfile=certificate_file, server_side=True)
 
@@ -161,41 +181,56 @@ def start_https_server(listening_port, basic_authentication_key, certificate_fil
     except KeyboardInterrupt:
         print("\n[!] Keyboard interrupt received, exiting...")
         https_server.server_close()
+        auth_logfile.close()
         sys.exit(0)
 
+# Print usage data
+def usage():
+    print('\np3u.py -l ip -p port -a user:password -c server.pem')
 
+# Main function
 if __name__ == '__main__':
+    # Try to parse arguments
     try:
         opts, args = getopt.getopt(sys.argv[1:], "ghl:p:a:c:")
+
+    # Error Handling
     except getopt.GetoptError as err:
         print(err)
-        print('\np3u.py -l ip -p port -a user:password -c server.pem \nGenerate cert file with: \n\
-openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes -subj "/C=/ST=/O=/OU=/CN="')
+        usage()
         sys.exit(2)
+
+    # Default options
     certificate_file = False
     use_auth = False
     basic_authentication_key = ""
     host = "127.0.0.1"
     listening_port = 8080
+
+    # Parse arguments
     for opt, arg in opts:
-        if opt in ('-h', '--help', '/h'):
-            print('\np3u.py -l ip -p port -a user:password -c server.pem \nGenerate cert file with: \n\
-openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes -subj "/C=/ST=/O=/OU=/CN="')
+        if opt in ('-h', '--help'):
+            # Print Help
+            usage()
             sys.exit(0)
         elif opt == '-l':
+            # Set listening IP
             host = arg
         elif opt == '-p':
+            # Set listening port
             listening_port = int(arg)
         elif opt == '-a':
+            # Enable basic authentication
             basicAuthString = arg
             basic_authentication_key = base64.b64encode(basicAuthString.encode("utf-8"))
             use_auth = True
         elif opt == '-c':
+            # Set Certificate file
             certificate_file = arg
         elif opt == '-g':
-            #os.system('openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes -subj "/C=/ST=/O=/OU=/CN="')
+            # Generate certificate
             os.system('openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout server.pem -out server.pem -subj "/C=YY"')
 
-    encrypted = "https://" if certificate_file else "http://"
-    print("[+] Staring server... " + encrypted + host + ":" + str(listening_port))
+    protocol = "https://" if certificate_file else "http://"
+    print("[+] Staring server... " + protocol + host + ":" + str(listening_port))
     start_https_server(listening_port, basic_authentication_key, certificate_file)
